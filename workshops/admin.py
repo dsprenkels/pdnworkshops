4b317aba78db7595038af9c28510763a36319c7f
Daan Sprenkels <dsprenkels@gmail.com>
2015-07-26 03:56:13 +0200

    Cleared up the admin interface

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from models import Workshop, User, WorkshopRating
+from .models import Workshop, User, WorkshopRating
 
-for model in [Workshop, User, WorkshopRating]:
-    admin.site.register(model, admin.ModelAdmin)
+class WorkshopRatingInline(admin.TabularInline):
+    model = WorkshopRating
+
+admin.register(admin.ModelAdmin, Workshop)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        WorkshopRatingInline
+    ]
--- workshops/models.py
+++ workshops/models.py
@@ -6,11 +6,17 @@ class Workshop(models.Model):
     max = models.IntegerField()
     # min = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.naam
+
 class User(models.Model):
     naam = models.CharField(max_length=100)
     email = models.CharField(max_length=100, blank=True)
     deleted = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.naam
 
 class WorkshopRating(models.Model):
     workshop = models.ForeignKey(Workshop)
